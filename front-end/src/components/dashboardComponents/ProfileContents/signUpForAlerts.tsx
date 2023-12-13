5b8b2c0295bdf17cceaeafe3b3ff12087ab54e56
Shreeya Patil <shreeya7p@gmail.com>
2023-12-13 13:37:07 -0500

    coded signUpForAlerts.tsx

--- front-end/src/components/dashboardComponents/ProfileContents/signUpForAlerts.tsx
+++ front-end/src/components/dashboardComponents/ProfileContents/signUpForAlerts.tsx
@@ -1,68 +1,25 @@
 import React from 'react'
 
-const SignUpForAlerts = () =>{
-    let tableheads=["Class","Description","Units","Grade","Grade Points"]
-    let gradeArray={
-        "Fall 2023":[{
-            "class":"COMPSCI520",
-            "description":"Software Engineering",
-            "units":"3",
-            "letter grade":"",
-            "grade points":""
-        },
-        {
-            "class":"COMPSCI687",
-            "description":"Reinforcement Learning",
-            "units":"3",
-            "letter grade":"",
-            "grade points":""
-        }],
-        "Spring 2024":[{
-            "class":"COMPSCI589",
-            "description":"Machine Learning",
-            "units":"3",
-            "letter grade":"A-",
-            "grade points":"11.1"
-        },
-        {
-            "class":"COMPSCI532",
-            "description":"Systems for Data Science",
-            "units":"3",
-            "letter grade":"A",
-            "grade points":"12"
-        }]
-    }
-    return <div className="course-history-section">
-        <div>
-            {
-               Object.entries(gradeArray).map((elem,index) =>{
-                   return <div>
-                                <div className="grade-term-head">{elem[0]}</div>
-                                <table className="ch-table">
-                                    <thead>
-                                        <tr className="ch-rowhead">
-                                            {tableheads.map((elem, index) => (
-                                                    <td className="ch-row-head-elem">{elem}</td>
-                                            ))}
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {elem[1].map((entry:any,index:any)=>{
-                                            return <tr key={index} className="ch-row">
-                                                <td className="ch-elem">{entry.class}</td>
-                                                <td className="ch-elem">{entry.description}</td>
-                                                <td className="ch-elem">{entry.units}</td>
-                                                <td className="ch-elem">{entry["letter grade"]}</td>
-                                                <td className="ch-elem">{entry["grade points"]}</td>
-                                        </tr>
-                                        })}
-                                    </tbody>
-                                </table>
-                            </div>
-               })
-            }
-        </div>
-    </div>
+const SignUpForAlerts = (props:any) => {
+    return <div  className="profile-right-section" >
+                <div>
+                    <div className="profile-label">
+                    Cell Phone Number to Receive Text Messages:
+                    </div>
+                    <div className="profile-content">
+                        +1413224765
+                    </div>
+                </div>
+                <div>
+                    <div className="profile-label">
+                    Email Addresses to Receive Emergency Emails:
+                    </div>
+                    <div className="profile-content">
+                        johndoe@umass.edu <br />
+                        jackjill@umass.edu
+                    </div>
+                </div>
+                
+            </div>
 }
-
 export default SignUpForAlerts
