928e04f34aebe20aa73723c314d52d8fca96e057
Shreeya Patil <shreeya7p@gmail.com>
2023-12-13 14:00:00 -0500

    ITAccountsAndPassword.tsx done

--- front-end/src/components/dashboardComponents/ProfileContents/ITAccountsAndPassword.tsx
+++ front-end/src/components/dashboardComponents/ProfileContents/ITAccountsAndPassword.tsx
@@ -1,68 +1,33 @@
 import React from 'react'
 
-const ITAccountsAndPassword = () =>{
-    let tableheads=["Class","Description","Units","Grade","Grade Points"]
-    let gradeArray={
-        "Fall 2023":[{
-            "class":"COMPSCI520",
-            "description":"Software Engineering",
-            "units":"3",
-            "letter grade":"",
-            "grade points":""
-        },
-        {
-            "class":"COMPSCI687",
-            "description":"Reinforcement Learning",
-            "units":"3",
-            "letter grade":"",
-            "grade points":""
-        }],
-        "Spring 2024":[{
-            "class":"COMPSCI589",
-            "description":"Machine Learning",
-            "units":"3",
-            "letter grade":"A-",
-            "grade points":"11.1"
-        },
-        {
-            "class":"COMPSCI532",
-            "description":"Systems for Data Science",
-            "units":"3",
-            "letter grade":"A",
-            "grade points":"12"
-        }]
-    }
-    return <div className="course-history-section">
-        <div>
-            {
-               Object.entries(gradeArray).map((elem,index) =>{
-                   return <div>
-                                <div className="grade-term-head">{elem[0]}</div>
-                                <table className="ch-table">
-                                    <thead>
-                                        <tr className="ch-rowhead">
-                                            {tableheads.map((elem, index) => (
-                                                    <td className="ch-row-head-elem">{elem}</td>
-                                            ))}
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {elem[1].map((entry:any,index:any)=>{
-                                            return <tr key={index} className="ch-row">
-                                                <td className="ch-elem">{entry.class}</td>
-                                                <td className="ch-elem">{entry.description}</td>
-                                                <td className="ch-elem">{entry.units}</td>
-                                                <td className="ch-elem">{entry["letter grade"]}</td>
-                                                <td className="ch-elem">{entry["grade points"]}</td>
-                                        </tr>
-                                        })}
-                                    </tbody>
-                                </table>
-                            </div>
-               })
-            }
-        </div>
-    </div>
+const ITAccountsAndPassword = (props:any) => {
+    return <div  className="profile-right-section" >
+                <div>
+                    <div className="profile-label">
+                    My IT Account:
+                    </div>
+                    <div className="profile-content">
+                        Account Name: johndoe <br />
+                        Type: NetID <br />
+                        Date Created: 04/24/2021 <br />
+                        Password Changed: 10/13/23 <br />
+                        Email Type: Google <br />
+                    </div>
+                </div>
+                <div>
+                    <div className="profile-label">
+                    UCard / Library Barcode:
+                    </div>
+                    <div className="profile-content">
+                        Spire ID: 12345678 <br />
+                        Name: John Doe <br />
+                        Card Type: Student <br />
+                        Run / ISO Number: 123456-123456789-1 <br />
+                        Library Barcode: 123456789123456 <br />
+                    </div>
+                </div>
+                
+            </div>
 }
 
 export default ITAccountsAndPassword
