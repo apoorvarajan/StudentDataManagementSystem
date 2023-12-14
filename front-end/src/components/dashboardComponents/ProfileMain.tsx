e1366e36866cc1befd92f784628cddc8cd052c6d
apoorvarajan <apoorvarajan1997@gmail.com>
2023-12-13 19:45:27 -0500

    profile section

--- front-end/src/components/dashboardComponents/ProfileMain.tsx
+++ front-end/src/components/dashboardComponents/ProfileMain.tsx
@@ -5,38 +5,29 @@ import '../../styles/profile.css';
 import SignUpForAlerts from './ProfileContents/signUpForAlerts';
 import PersonalInformation from './ProfileContents/personalInformation';
 import ITAccountsAndPassword from './ProfileContents/ITAccountsAndPassword';
+import SideBar from '../sideBar'
 
 
 const ProfileMain = (props: any) => {
-  const sections = [
-    'Sign Up For Alerts',
-    'Personal Information',
-    'IT Accounts and Password',
-  ];
-
-  const [sectionId, selectSection] = useState('Profile Information');
-
+  const [hash,hashChange] = useState(window.location.hash)
+  window.onhashchange = () =>{
+    hashChange(window.location.hash)
+  }
   return (
-    <div>
-      <div className="home-header-wrap">
-        <div className="home-heading-text">{sectionId}</div>
-      </div>
-      <div className="profile-section-wrap">
-        <div className="profile-left-section">
-          {sections.map((item) => (
-            <div
-              key={item}
-              className="profile-sec-item"
-              onClick={() => selectSection(item)}
-            >
-              {item}
-            </div>
-          ))}
+    <div className="profile-section-wrap">
+        <SideBar/>
+      <div className="right-sec">
+        <div>
+          <div className="general-heading">{hash === "#alert" ? 'Sign Up For Alerts':
+                hash === "#itacc" ? 'IT Accounts and Password' : 
+                'Personal Information'}
+          </div>
         </div>
-        {sectionId === 'Sign Up For Alerts' && <SignUpForAlerts />}
-        {sectionId === 'Personal Information' && <PersonalInformation />}
-        {sectionId === 'IT Accounts and Password' && <ITAccountsAndPassword />}
-
+        <div>
+          {hash === "#alert" ? <SignUpForAlerts />:
+          hash === "#itacc" ? <ITAccountsAndPassword /> : 
+          <PersonalInformation />}
+        </div>
       </div>
     </div>
   );
--- front-end/src/components/sideBar.tsx
+++ front-end/src/components/sideBar.tsx
@@ -1,11 +1,39 @@
 import React, {useState} from 'react'
 
-const sideBar = () =>{
-    const [sectionId, selectSection] = useState("Academic Summary");
-    const sections =["Academic Summary","Grades","Course History","Transcript Request","Graduation"]
-    return <div className="academics-left-section">
-                {sections.map((item)=>{
-                    return <div className="academic-sec-item" onClick={()=> selectSection(item)}>{item}</div>
+const SideBar = () =>{
+    let dashBoardSections = ["Profile","Academics","Course Planning Assistant"]
+    let dashboardList=[
+        [{
+            'Sign Up For Alerts':"/profile#alert",
+            'Personal Information':"/profile#pInfo",
+            'IT Accounts and Password':"/profile#itacc"
+        }],
+        [{
+            "Academic Summary":"/academics#summary",
+            "Grades":"/academics#grades",
+            "Course History":"/academics#chist",
+            "Transcript Request":"/academics#treq",
+            "Graduation":"/academics#grad"
+        }],
+        [{
+            "Current Courses":"/cpa#ccourses",
+            "Check Requirement":"/cpa#req",
+            "Browse all courses":"/cpa#browse"
+        }]
+    ]
+    return <div className="sidebar-section">
+                {dashBoardSections.map((item,key)=>{
+                    return <div>
+                                <div className="academic-sec-item">{item}</div>
+                                {dashboardList[key].map((subitem:any)=>{
+                                    return Object.entries(subitem).map(([key,value])=>{
+                                        const url = value as string
+                                        return <div className="sideBar-elem" onClick={()=> window.location.href=url}>{key}</div>
+                                    })
+                                })}
+                            </div>
                 })}
             </div>
 }
+
+export default SideBar
