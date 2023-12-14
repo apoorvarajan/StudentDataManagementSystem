c6a926211e0ccc7967b79efdad7bf2953b1a43d8
apoorvarajan <apoorvarajan1997@gmail.com>
2023-12-14 18:59:40 -0500

    design requirement

--- client/src/components/dashboardComponents/CpaContents/checkReq.tsx
+++ client/src/components/dashboardComponents/CpaContents/checkReq.tsx
@@ -29,14 +29,15 @@ class CheckReq extends React.Component<any,any>{
         })
     }
     render(){
-        return <div>
-            <div>
+        return <div  className="academics-right-section" >
+            {this.state.req && <div className="req-result">{"Satisfies "+this.state.req.progress+" requirements"}</div>}
+            <div className="how-many-sec">
                 <div> How many courses do you want to check ?</div>
-                <input name="noReq" id="noReq" type="number"></input>
-                <div onClick={()=>this.noCourses()}>Submit</div>
+                <input name="noReq" id="noReq" type="number" className="how-input"></input>
+                <div onClick={()=>this.noCourses()}  className="login-modal-button b">Submit</div>
             </div>
             {this.state.no > 0 && (
-                <div>
+                <div className="how-many-sec">
                     <div> Enter course numbers </div>
                     {Array.from({ length: this.state.no }).map((_, index) => (
                     <input
@@ -44,12 +45,12 @@ class CheckReq extends React.Component<any,any>{
                         type="text"
                         id={`input${index + 1}`}
                         value={this.state.inputValues[index]}
+                        className="courseno-input"
                     />
                     ))}
-                    <div onClick={()=>this.checkReq()}> Check Requirement</div>
+                    <div onClick={()=>this.checkReq()}  className="login-modal-button b"> Check Requirement</div>
                 </div>
             )}
-            {this.state.req && <div>{"Satisfies "+this.state.req.progress+" requirements"}</div>}
         </div>
     }
 }
