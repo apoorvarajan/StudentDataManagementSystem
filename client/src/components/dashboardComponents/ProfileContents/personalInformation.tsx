cc2680cb4a508bae2270fc0fc92c52990533e302
apoorvarajan <apoorvarajan1997@gmail.com>
2023-12-14 21:39:21 -0500

    tsconfig fix

--- client/src/components/dashboardComponents/ProfileContents/personalInformation.tsx
+++ client/src/components/dashboardComponents/ProfileContents/personalInformation.tsx
@@ -7,33 +7,18 @@ const PersonalInformation = (props:any) => {
                         Name:
                     </div>
                     <div className="profile-content">
-<<<<<<< HEAD
-                        John Doe
-                    </div>
-                </div>
-                
-=======
                         {props.sdetails.name.firstName + " " + (props.sdetails.name.middleName ? (props.sdetails.middleName + " ") : "") + props.sdetails.name.lastName}
                     </div>
                 </div>
->>>>>>> f865a432b73b74079a1da929dcb1ed525ead011e
                 <div className="pInfo-sec">
                     <div className="profile-label">
                         Address:
                     </div>
                     <div className="profile-content">
-<<<<<<< HEAD
-                        880, South Pleasant Street, Apt. No. 11 <br/>
-                        Amherst, MA, 01002
-                    </div>
-                </div>
-                
-=======
                         {props.sdetails.address.line1} , {props.sdetails.address.city} , {props.sdetails.address.state}<br/>
                         zipcode: {props.sdetails.address.zip}
                     </div>
                 </div>
->>>>>>> f865a432b73b74079a1da929dcb1ed525ead011e
                 <div>
                     <div className="profile-label">
                         Phone:
