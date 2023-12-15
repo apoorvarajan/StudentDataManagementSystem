7f4789c2f9fe71bb55f4955b800743efddcf3d47
Kartik Choudhary <kartikchoudhary9@gmail.com>
2023-12-15 02:14:41 +0000

    Add backend documentation

--- back-end/src/sdms_server/authentication/authentication.py
+++ back-end/src/sdms_server/authentication/authentication.py
@@ -2,17 +2,22 @@ import re
 import bcrypt
 import hashlib
 import base64
+from argon2 import PasswordHasher
+from argon2.exceptions import Argon2Error
 
-class InvalidPasswordError(Exception):
-    DEFAULT_ERROR_MESSAGE = (
-        'Password must be at least 8 characters long, contain at least '
-        'one uppercase letter, one lowercase letter, one number, and '
-        'one special character from [@, $, !, %, *, ?, &]')
-    
-    def __init__(self,  *args, msg=DEFAULT_ERROR_MESSAGE, **kwargs):
-        super().__init__(msg, *args, **kwargs)
+from sdms_server.exceptions.exceptions import InvalidPasswordError
+
 
 def hash_password(password:str)->str:
+    """
+    Hashes the given password using SHA-256, base64 encoding, and bcrypt hashing.
+
+    Args:
+        password (str): The password to be hashed.
+
+    Returns:
+        str: The bcrypt hash of the password.
+    """
     # Hash with SHA-256
     sha_hash = hashlib.sha256(password.encode('utf-8')).digest()
 
@@ -25,7 +30,29 @@ def hash_password(password:str)->str:
     # Return the bcrypt hash as string
     return bcrypt_hash.decode('utf-8')
 
+def hash_password_argon2(password:str)->str:
+    """
+    Hashes the given password using Argon2 algorithm.
+
+    Args:
+        password (str): The password to be hashed.
+
+    Returns:
+        str: The Argon2 hash of the password.
+    """
+    return PasswordHasher().hash(password)
+
 def check_password(password:str, hashed:str)->bool:
+    """
+    Checks if the given password matches the hashed password.
+
+    Args:
+        password (str): The password to be checked.
+        hashed (str): The hashed password to compare against.
+
+    Returns:
+        bool: True if the password matches the hashed password, False otherwise.
+    """
     # Hash with SHA-256
     sha_hash = hashlib.sha256(password.encode('utf-8')).digest()
 
@@ -35,7 +62,32 @@ def check_password(password:str, hashed:str)->bool:
     # Check bcrypt hashed password
     return bcrypt.checkpw(hash_b64, hashed.encode('utf-8'))
 
+def check_password_argon2(password:str, hashed:str)->bool:
+    """
+    Checks if the given password matches the Argon2 hashed password.
+
+    Args:
+        password (str): The password to be checked.
+        hashed (str): The Argon2 hashed password to compare against.
+
+    Returns:
+        bool: True if the password matches the Argon2 hashed password, False otherwise.
+    """
+    try:
+        return PasswordHasher().verify(hashed, password)
+    except Argon2Error:
+        return False
+
 def validate_password(password:str):
+    """
+    Validates the password against a regex pattern.
+
+    Args:
+        password (str): The password to be validated.
+
+    Raises:
+        InvalidPasswordError: If the password does not meet the required criteria.
+    """
     regex = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$'
     if not re.match(regex, password):
         raise InvalidPasswordError()
--- back-end/src/sdms_server/authorization/decorator.py
+++ back-end/src/sdms_server/authorization/decorator.py
@@ -4,6 +4,15 @@ import jwt
 from sdms_server.exceptions.exceptions import UnauthorizedError
 
 def decode_token(token:str, /)->dict:
+    """
+    Decode a JWT token.
+
+    Args:
+        token (str): The JWT token to decode.
+
+    Returns:
+        dict: The decoded token as a dictionary.
+    """
     decoded = jwt.decode(
         token, os.getenv('JWT_SECRET'),
         algorithms=[os.getenv('JWT_ALGORITHM')])
@@ -15,7 +24,6 @@ def permissions_required(**permissions):
     """Decorator to check permissions before running a function.
 
     Args:
-
         **permissions: The permissions for different roles required 
             to run the function.
 
@@ -28,15 +36,13 @@ def permissions_required(**permissions):
                 decoded_token = decode_token(_token)
             except (jwt.exceptions.PyJWTError, AssertionError) as e:
                 raise UnauthorizedError('Invalid token') from e
-            print(f'Checking permissions for user `{decoded_token["user_id"]}` with role `{decoded_token["role"]}`...')
             if decoded_token['role'] not in permissions:
                 raise UnauthorizedError('Role not found')
-            
+
             for check in permissions[decoded_token['role']]:
                 if not check(decoded_token['user_id'], **kwargs):
                     raise UnauthorizedError('User not allowed')
 
-            print(f'User `{decoded_token["user_id"]}` has permissions')
             return func(decoded_token, **kwargs)
         return wrapper
     return decorator
