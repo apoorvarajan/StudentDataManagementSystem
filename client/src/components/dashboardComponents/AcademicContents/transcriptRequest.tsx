6db53c1372ea461873fee798fdf94a70e4c5fe01
apoorvarajan <apoorvarajan1997@gmail.com>
2023-12-14 20:23:38 -0500

    readme

--- client/src/components/dashboardComponents/AcademicContents/transcriptRequest.tsx
+++ client/src/components/dashboardComponents/AcademicContents/transcriptRequest.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
 
-const TranscriptRequest = () => {
-    return <div  className="academics-right-section">
-        <div className="academic-content">
-            Please contact your advisor to request for transcript
-        </div>
+class TranscriptRequest extends React.Component<any,any>{
+    constructor(props:any){
+        super(props)
+        this.state={
+            tReq:false
+        }
+    }
+    reqTranscript = () => {
+        this.setState({
+            tReq:true
+        })
+    }
+    render(){
+        return <div  className="academics-right-section">
+        {!this.state.tReq && <div onClick={()=> this.reqTranscript()} className="login-modal-button blah"> Request for Transcript</div>}
+        {this.state.tReq && <div className="academic-content">
+            Your Transcript request has been sent. You will recieve it in your school email in 2 business days
+        </div>}
     </div>
+    }
 }
 
 export default TranscriptRequest
