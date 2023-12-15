8a2301798e5c99b5dfa7eecf2f5c06257b68195e
apoorvarajan <apoorvarajan1997@gmail.com>
2023-12-14 19:09:08 -0500

    browse design

--- client/src/components/dashboardComponents/CpaContents/browseCourses.tsx
+++ client/src/components/dashboardComponents/CpaContents/browseCourses.tsx
@@ -27,20 +27,20 @@ class BrowseCourses extends React.Component<any,any>{
         let curCourseArray:any = []//this.props.scourses.courseList
         let allCourses = this.state.allCourses
     return <div className="course-history-section">
-        <div>
-            <div>
+        <div className="browse-top">
+            <div className="bt-sec">
                 <label htmlFor="dept">Choose Department:</label>
-                <select name="dept" id="dept">
+                <select name="dept" id="dept" className="select-bt">
                     <option value="volvo">COMPSCI</option>
                 </select>
             </div>
-            <div>
+            <div className="bt-sec">
                 <label htmlFor="deg">Choose Degree</label>
-                <select name="deg" id="deg">
+                <select name="deg" id="deg" className="select-bt">
                     <option value="volvo">MS</option>
                 </select>
             </div>
-            <div onClick={()=>this.submitBrowse()}>
+            <div onClick={()=>this.submitBrowse()}  className="login-modal-button k">
                 Submit
             </div>
         </div>
